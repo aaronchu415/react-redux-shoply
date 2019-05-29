3393d0e08546a227a353a32ed91e65b8adee68e3
aaronchu415 <aaronchu415@gmail.com>
2019-05-29 13:17:43 -0700

    commit

--- src/container/ItemContainer.js
+++ src/container/ItemContainer.js
@@ -2,7 +2,6 @@ import { connect } from 'react-redux'
 import Item from '../components/Item';
 import { addItem, removeItem } from '../redux/actions';
 
-
 function mapStateToProps(state) {
 
   return {
